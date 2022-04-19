3e686cde7d82903814108061e6d08cdba255d8c3
immsee098 <immsee098@gmail.com>
2022-04-20 03:19:29 +0900

    Tue와 함께

--- src/main/java/com/week/two/weektwomon/MyHandler.java
+++ src/main/java/com/week/two/weektwomon/MyHandler.java
@@ -1,28 +1,37 @@
 package com.week.two.weektwomon;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
+import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class MyHandler {
 
+    WebClient webClient = WebClient.create("http://localhost:8081");
+
     public Mono<ServerResponse> hello (ServerRequest req) {
         String name = req.queryParam("name").get();
-        HashMap nameMap = new HashMap();
-        nameMap.put("to", name);
-        nameMap.put("message", "hello "+name);
-        Mono<HashMap> mymono = Mono.just(nameMap);
+
+        //https://stackoverflow.com/questions/56314853/issue-with-spring-webflux-webclient-nothing-happens-when-trying-to-send-post-r
+        //retrieve는 subscibe를 해주는 역할이 아닌지? retrieve는 날리기만 하는 기능인 것으로... 섭스크라이브나 블락을 따로 안해주면 섭스크라이브가 안되는 모양...
+        Mono result = webClient.get()
+                .uri("/getJob?name="+name)
+                .retrieve()
+                .bodyToMono(HashMap.class);
 
         return ServerResponse
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromProducer(mymono, HashMap.class));
+                .body(BodyInserters.fromProducer(result, HashMap.class));
     }
 }
--- src/main/java/com/week/two/weektwomon/MyRouter.java
+++ src/main/java/com/week/two/weektwomon/MyRouter.java
@@ -3,27 +3,24 @@ package com.week.two.weektwomon;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.HashMap;
 
 
 @Configuration
 public class MyRouter {
-//    @Bean
-//    public RouterFunction<ServerResponse> route2(MyHandler myHandler) {
-//        return RouterFunctions.route()
-//                .path("/test1", builder ->
-//                        builder.GET("", myHandler::hello))
-//                .build();
-//    }
-//
-    
     @Bean
     public RouterFunction<ServerResponse> route(MyHandler handler) {
-        return RouterFunctions.route(
-                RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::hello);
+        return RouterFunctions.route()
+                .GET("/hello", handler::hello)
+                .build();
     }
 }
